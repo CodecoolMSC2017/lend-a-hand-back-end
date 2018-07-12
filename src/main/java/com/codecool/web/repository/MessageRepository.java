a8f5edb5691d461a07a29729f97946be15cb83e8
AlexaPekar <missdreambelle@gmail.com>
2018-07-12 19:10:10 +0200

    Implement MessageRepository and MessageService

---
+++ src/main/java/com/codecool/web/repository/MessageRepository.java
@@ -0,0 +1,12 @@
+package com.codecool.web.repository;
+
+import com.codecool.web.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+
+    List<Message> findAllBySender_IdAndReceiver_IdOrderByTimestampAsc(int senderId, int receiverId);
+    List<Message> findAllBySender_IdAndReceiver_IdAndTextContainingOrderByTimestampAsc(int senderId, int receiverId, String keyword);
+}
---
+++ src/main/java/com/codecool/web/service/MessageService.java
@@ -0,0 +1,22 @@
+package com.codecool.web.service;
+
+import com.codecool.web.model.Message;
+import com.codecool.web.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    List<Message> getAllBySender_IdAndReceiver_IdOrderByTimestampAsc(int senderId, int receiverId) {
+        return messageRepository.findAllBySender_IdAndReceiver_IdOrderByTimestampAsc(senderId, receiverId);
+    }
+    List<Message> getAllBySender_IdAndReceiver_IdAndTextContainingOrderByTimestampAsc(int senderId, int receiverId, String keyword) {
+        return messageRepository.findAllBySender_IdAndReceiver_IdAndTextContainingOrderByTimestampAsc(senderId, receiverId, keyword);
+    }
+}
