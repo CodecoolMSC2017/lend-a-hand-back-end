7ff9ff4a387aff8ebde1c551a5965ceee2ffe630
AlexaPekar <missdreambelle@gmail.com>
2018-07-12 16:06:04 +0200

    Insert annotation above findAll() method in UserRepository

--- src/main/java/com/codecool/web/repository/UserRepository.java
+++ src/main/java/com/codecool/web/repository/UserRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @Override
     List<User> findAll();
     User findById(int id);
     User findByEmail(String email);
