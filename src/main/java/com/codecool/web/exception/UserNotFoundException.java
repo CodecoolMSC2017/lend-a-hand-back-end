d7b316aff9633eef75cbc93595b4fbfd2b7ba28a
AlexaPekar <missdreambelle@gmail.com>
2018-07-12 11:43:38 +0200

    Complete exception classes with annotations

--- src/main/java/com/codecool/web/exception/UserNotFoundException.java
+++ src/main/java/com/codecool/web/exception/UserNotFoundException.java
@@ -1,4 +1,8 @@
 package com.codecool.web.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No user found")
 public class UserNotFoundException extends Exception {
 }
--- src/main/java/com/codecool/web/exception/WrongPasswordException.java
+++ src/main/java/com/codecool/web/exception/WrongPasswordException.java
@@ -1,4 +1,8 @@
 package com.codecool.web.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.CONFLICT, reason="Wrong password entered")
 public class WrongPasswordException extends Exception {
 }
