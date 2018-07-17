e94f1e21dec886210bace469c01267bc8ba000aa
AlexaPekar <missdreambelle@gmail.com>
2018-07-17 11:42:32 +0200

    Fix exception handling in register method in GuestController

--- src/main/java/com/codecool/web/controller/GuestController.java
+++ src/main/java/com/codecool/web/controller/GuestController.java
@@ -6,13 +6,11 @@ import com.codecool.web.exception.WrongPasswordException;
 import com.codecool.web.model.User;
 import com.codecool.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("")
@@ -24,13 +22,9 @@ public class GuestController {
     @PostMapping(path = "/register",
         consumes = MediaType.APPLICATION_JSON_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE)
-    public String register(@RequestBody User user) {
-        try {
-            service.registerUser(user);
-            return "You are successfully registered";
-        } catch (UserAlreadyRegisteredException ex) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "You are already registered, please log in!");
-        }
+    public String register(@RequestBody User user) throws UserAlreadyRegisteredException {
+        service.registerUser(user);
+        return "You are successfully registered";
     }
 
     @PostMapping(path = "/login",
