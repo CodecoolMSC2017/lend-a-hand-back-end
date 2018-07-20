8c2a142d57826ae89f6c43189c511ccad551fd99
Farago Bence <semmiertelme13@gmail.com>
2018-07-20 07:52:36 +0200

    User already Exception now return different message.

--- src/main/java/com/codecool/web/exception/UserAlreadyRegisteredException.java
+++ src/main/java/com/codecool/web/exception/UserAlreadyRegisteredException.java
@@ -3,6 +3,6 @@ package com.codecool.web.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value=HttpStatus.CONFLICT, reason="This email is already in use")
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "This email or user name already in use")
 public class UserAlreadyRegisteredException extends Exception{
 }
