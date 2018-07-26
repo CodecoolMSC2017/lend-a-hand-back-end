d78646e166072ecd8086fd0e905084f86db71e44
krisztianKollar <kollarkr@gmail.com>
2018-07-26 15:00:21 +0200

    Begin implementing tests.

---
+++ src/main/Test/com/codecool/web/service/AdServiceTest.java
@@ -0,0 +1,70 @@
+package com.codecool.web.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getAllByAdvertiserId() {
+    }
+
+    @Test
+    void getAllByCategory() {
+    }
+
+    @Test
+    void getAllByType() {
+    }
+
+    @Test
+    void getAllByCategoryAndType() {
+    }
+
+    @Test
+    void getAllByTitleOrDescriptionContaining() {
+    }
+
+    @Test
+    void getAllByKeywordAndCategory() {
+    }
+
+    @Test
+    void getAllByKeywordAndType() {
+    }
+
+    @Test
+    void getAllByKeywordAndCategoryAndType() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void addNewAd() {
+    }
+
+    @Test
+    void deleteAd() {
+    }
+
+    @Test
+    void updateAdData() {
+    }
+}
