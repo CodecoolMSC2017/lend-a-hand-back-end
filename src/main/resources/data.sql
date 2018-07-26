821c0ad2e92cedce4afe09adb0d2c99ea5edc0d7
krisztianKollar <kollarkr@gmail.com>
2018-07-26 14:32:40 +0200

    More ads in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,7 +17,13 @@ VALUES (1, 2, 'Looking for babysitter', 'Want a cute person to look after my chi
 (2, 1, 'K&S Express Mowing Service', 'K&S Express Mowing Service offers a great friendly service along with a Affordable Price.', 55, 'Garden', true, '2018-8-08 10:23:54', 'Offer'),
 (2, 1, 'Plastering & Rendering', 'We Provide all services to do with plastering and rendering across the Staffordshire area.', 55, 'Construction', true, '2018-8-08 12:23:54', 'Offer'),
 (2, 1, 'Coding lessons', 'Coding lessons available in Java, C#, C++ languages', 55, 'Education', true, '2018-8-09 10:23:54', 'Offer'),
-(3, 2, 'Searching for gardener', 'Hi, We need a team of 2 professional Gardeners! We want them to be really responsible and gentle with our amazing and beautiful plants. They are our life. So let us know, if you find yourselves appropriate. Cheers!', 44, 'Garden', false, '2018-8-10 19:23:54', 'Hire');
+(3, 2, 'Searching for gardener', 'Hi, We need a team of 2 professional Gardeners! We want them to be really responsible and gentle with our amazing and beautiful plants. They are our life. So let us know, if you find yourselves appropriate. Cheers!', 44, 'Garden', false, '2018-8-10 19:23:54', 'Hire'),
+(3, null, 'Indian runner ducks wanted', 'We need indian runner ducks to collect all of the slugs in our backyard! We need them a couple of weeks until they do the work!', 21, 'Garden', false, '2018-8-12 19:43:24', 'Hire'),
+(1, 1, 'Hobbit house for you!', 'Would you like to live your life in a hobbit house? We can make it for you! If you have only little money, you can order a little house! It is guaranted without vertical lines and right angles!', 555, 'Construction', true, '2015-8-18 16:29:24', 'Offer'),
+(1, null, 'Fairy cleaning!', 'I can clean your house wearing high-heels made of glass! It will be fairy!', 65, 'Housework', false, '2017-9-21 12:27:43', 'Offer'),
+(3, null, 'Here comes the king of the vacuumcleaners!', 'I am the fastest cleaner with a vacuumcleaner! I can clean your house within an hour! If I am not ready in 60 minutes, you pay nothing and my work is free for you!', 25, 'Housework', true, '2017-2-25 06:09:24', 'Offer'),
+(2, 1, 'How to write a poem', 'You should be John the Golden, it is cool by the girls! Do you know about versification and rhymes? You can learn it like a real poet!', 1, 'Education', false, '2011-4-01 00:00:01', 'Offer');
+
 
 INSERT INTO authorities (username, authority) values ('Beagun', 'ADMIN'),
                                                     ('Lorincke23','USER'),
