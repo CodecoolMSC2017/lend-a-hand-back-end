a2758efb983049a4f9bf8a3e5062c3e5bdf79ec1
Farago Bence <semmiertelme13@gmail.com>
2018-08-07 10:09:40 +0200

    Added basic message data.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -73,3 +73,26 @@ VALUES (1,2,2),
 (4,1,5),
 (3,1,2);
 
+INSERT INTO messages (sender_id, receiver_id, text, timestamp)
+VALUES (1,2,'Hello, I am Beagun can i ask about the payment?','2018-8-08 10:23:54'),
+(2,1,'Hi, Of course i can pay 20$/ minute for it.','2018-8-08 10:24:05'),
+(1,2,'It is wonderful!','2018-8-08 10:24:20'),
+(1,2,'So when can i go and do the work for you?','2018-8-08 10:24:44'),
+(2,1,'Is this afternoon okay for you?','2018-8-08 10:24:55'),
+(1,2,'Of course, i will be there at 6pm','2018-8-08 10:25:20' ),
+(2,1,'Fine, thanks!','2018-8-08 10:25:30'),
+
+(1,4,'Hello, I am Beagun can i ask about the payment?','2018-8-08 10:23:54'),
+(4,1,'Hi, Of course i can pay 20$/ minute for it.','2018-8-08 10:24:05'),
+(1,4,'It is wonderful!','2018-8-08 10:24:20'),
+(1,4,'So when can i go and do the work for you?','2018-8-08 10:24:44'),
+(4,1,'Is this afternoon okay for you?','2018-8-08 10:24:55'),
+(1,4,'Of course, i will be there at 6pm','2018-8-08 10:25:20' ),
+(4,1,'Fine, thanks!','2018-8-08 10:25:30');
+
+
+
+
+
+
+
