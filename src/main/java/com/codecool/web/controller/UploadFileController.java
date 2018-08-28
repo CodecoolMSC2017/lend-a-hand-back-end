5560525683231645263bebb8938df3acbbe5630a
alexapekar <LolkaBolka72>
2018-08-28 11:50:18 +0200

    Implement Amazon S3 webservice configuration and connection backend

---
+++ src/main/java/com/codecool/web/controller/UploadFileController.java
@@ -0,0 +1,24 @@
+package com.codecool.web.controller;
+
+import com.codecool.web.service.S3Services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class UploadFileController {
+
+	@Autowired
+    S3Services s3Services;
+
+    @PostMapping("/api/file/upload")
+    public String uploadMultipartFile(@RequestParam("file") MultipartFile file) {
+    	String keyName = file.getOriginalFilename();
+		s3Services.uploadFile(keyName, file);
+		return "Upload Successfully -> KeyName = " + keyName;
+    }
+}
