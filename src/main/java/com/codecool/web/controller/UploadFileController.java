74d2b42124c4857097035ccdc5a52e77aaab92a5
alexapekar <LolkaBolka72>
2018-08-28 14:00:26 +0200

    Fix typing issues in application.properties and UploadFileController

--- src/main/java/com/codecool/web/controller/UploadFileController.java
+++ src/main/java/com/codecool/web/controller/UploadFileController.java
@@ -2,20 +2,17 @@ package com.codecool.web.controller;
 
 import com.codecool.web.service.S3Services;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/file")
 public class UploadFileController {
 
 	@Autowired
     S3Services s3Services;
 
-    @PostMapping("/api/file/upload")
+    @PostMapping("/upload")
     public String uploadMultipartFile(@RequestParam("file") MultipartFile file) {
     	String keyName = file.getOriginalFilename();
 		s3Services.uploadFile(keyName, file);
